87167d2cdfe20517b46393549236f2e09df23128
Your Name <41466655+Sajaalghoul@users.noreply.github.com>
2023-10-03 21:40:43 +0300

    delete product functionallity with testing

--- seller/seller.js
+++ seller/seller.js
@@ -7,7 +7,7 @@ const productImage = document.getElementById("image");
 const productCateogry = document.getElementById("category");
 const formInputs = document.querySelectorAll("input");
 const productsList = document.getElementById("productsList");
-const displayButton=document.getElementById("displayProducts");
+const displayButton = document.getElementById("displayProducts");
 //variables
 let products = [];
 
@@ -16,7 +16,13 @@ if (addForm) {
   addForm.addEventListener("submit", (event) => {
     event.preventDefault();
     getfromLocalStorage();
-    addProduct(productName, productDetail, productPrice, productImage,productCateogry);
+    addProduct(
+      productName,
+      productDetail,
+      productPrice,
+      productImage,
+      productCateogry
+    );
   });
 }
 
@@ -42,7 +48,13 @@ function formValidation(name, detail, price, image) {
   );
 }
 // //adding the product to the list , update id,getfromlocalstorage, push to localstorage
-function addProduct(productName, productDetail, productPrice,productImage,productCateogry) {
+function addProduct(
+  productName,
+  productDetail,
+  productPrice,
+  productImage,
+  productCateogry
+) {
   if (formValidation(productName, productDetail, productPrice, productImage)) {
     const name = productName.value;
     const price = productPrice.value;
@@ -65,18 +77,18 @@ function addProduct(productName, productDetail, productPrice,productImage,produc
   }
 }
 //display button event listener
-///display evnt listener ****not done
-displayButton.addEventListener('click',()=>
-{
-  getfromLocalStorage();
-  displayProducts(products);
+if (displayButton) {
+  displayButton.addEventListener("click", () => {
+    getfromLocalStorage();
+    displayProducts(products);
+  });
 }
-)
+
 //dispaly all products(addEvent)
 function displayProducts(products) {
-    productsList.innerHTML = "";
-    products.forEach((product) => {
-      productsList.innerHTML += `
+  productsList.innerHTML = "";
+  products.forEach((product) => {
+    productsList.innerHTML += `
           <div class="flex products__product" product-id=${product.id}>
               <img src=${product.image} alt="productImage" class="product__img">
               <p calss="product__name">${product.name}</p>
@@ -86,13 +98,41 @@ function displayProducts(products) {
               <button class="product__edit">Edit</button>
               <button class="product__delete">Delete</button>
           </div>`;
+  });
+  productEventLinstener();
+}
+// delete and edit eventlistener
+function productEventLinstener() {
+  const productContainers = document.querySelectorAll(".products__product");
+  productContainers.forEach((product) => {
+    product.addEventListener("click", (element) => {
+      if (element.target.className == "product__delete") {
+        deleteProduct(element.target.parentElement.getAttribute("product-id"));
+        element.target.parentElement.remove();
+      }
+      if (element.target.className == "product__edit") {
+        const productId =
+          element.target.parentElement.getAttribute("product-id");
+        products.forEach((product) => {
+          if (product.id == productId) {
+            editProduct(product);
+          }
+        });
+      }
     });
-    // productEventLinstener();
-  }
+  });
+}
+
+///delete function
+function deleteProduct(productId) {
+  products = products.filter((product) => product.id != productId);
+  addToLocalStorage(products);
+}
 ///////////
-function clearProducts() {//for testing
-    products = [];
-  }
+function clearProducts() {
+  //for testing
+  products = [];
+}
 
 /////exports
 // module.exports = {
@@ -100,5 +140,6 @@ function clearProducts() {//for testing
 //   getfromLocalStorage,
 //   addToLocalStorage,
 //   addProduct,
-//   clearProducts
+//   clearProducts,
+//   deleteProduct,
 // };
--- test.js
+++ test.js
@@ -4,6 +4,7 @@ const {
   addToLocalStorage,
   addProduct,
   clearProducts,
+  deleteProduct,
 } = require("./seller/seller");
 const localStorageMock = (() => {
   let store = {};
@@ -112,7 +113,7 @@ describe("formValidation", () => {
   });
 });
 
-//addProduct
+//addProductTesting
 describe("addProduct", () => {
   beforeEach(() => {
     localStorage.clear();
@@ -141,5 +142,29 @@ describe("addProduct", () => {
     );
     const products = getfromLocalStorage();
     expect(products.length).toBe(0);
+  });
+});
+///deleteProductTesting
+describe("deleteProduct", () => {
+  afterEach(() => {
+    // Clear localStorage afer test
+    localStorage.clear();
+  });
+  test("should delete product", () => {
+    const mockProduct = {
+      id: 0,
+      name: "car",
+      price: 1000,
+      detail: "white car",
+      image:
+        "https://res.cloudinary.com/carsxe/image/upload/f_auto,fl_lossy,q_auto/v1569282984/carsxe-api/purple_porsche.png",
+      category: "electronics",
+    };
+    addToLocalStorage([mockProduct]);
+    let products = getfromLocalStorage();
+    const length = products.length;
+    deleteProduct(products[0].id);
+    products = getfromLocalStorage();
+    expect(products.length).toBe(length - 1);
   });
 });
